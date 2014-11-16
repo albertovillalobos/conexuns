7d7a3fe0270cadbbef6bd859f4c8242de19d9f92
SOAN TEODOROWICZ <alberto@albertovillalobos.me>
2014-11-15 21:24:38 -0800

    first commit

---
+++ client/components/auth/user.service.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('ideaboardApp')
+  .factory('User', function ($resource) {
+    return $resource('/api/users/:id/:controller', {
+      id: '@_id'
+    },
+    {
+      changePassword: {
+        method: 'PUT',
+        params: {
+          controller:'password'
+        }
+      },
+      get: {
+        method: 'GET',
+        params: {
+          id:'me'
+        }
+      }
+	  });
+  });
